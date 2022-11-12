76d74668512afeef33e1a17e2fea1578d87f02dd
Ken Stamper <kenstamper@Kens-MacBook-Air.local>
2022-11-11 22:33:59 -0700

    add commands, instructions and integrate horoscope api

--- app.py
+++ app.py
@@ -1,22 +1,53 @@
 import requests
 
-BASE_URL = "https://ohmanda.com/api/horoscope/"
+class Session:
+    _base_url = "https://ohmanda.com/api/horoscope/"
 
-signs = [
-    "aquarius",
-    "pisces",
-    "aries",
-    "taurus",
-    "gemini",
-    "cancer",
-    "leo",
-    "virgo",
-    "libra",
-    "scorpio",
-    "sagittarius",
-    "capricorn",
-]
+    @classmethod
+    def begin(cls):
+        print("\n** Welcome to Daily Scope! **\n")
+        while True:
+            user_input = input("""Enter your sign to read today's horoscope.
+You can also enter 'done' to exit or 'zodiac' to see available signs.
 
-response = requests.get(BASE_URL + "scorpio")
+I'm waiting: """)
+            if user_input in Zodiac.get_signs():
+                horoscope = cls.get_horoscope(user_input)
+                print("\n** " + horoscope["sign"] + " - " + horoscope["date"] + " **")
+                print("\n" + horoscope["horoscope"] + "\n")
 
-print(response.json())
+            elif user_input == "done":
+                break
+            elif user_input == "zodiac":
+                print("\n================")
+                for sign in Zodiac.get_signs():
+                    print("- " +sign)
+                print("================\n")
+    
+    @classmethod
+    def get_horoscope(cls, sign):
+        response = requests.get(cls._base_url + sign)
+        return response.json()
+
+
+class Zodiac:
+    _signs = [
+        "aries",
+        "taurus",
+        "gemini",
+        "cancer",
+        "leo",
+        "virgo",
+        "libra",
+        "scorpio",
+        "sagittarius",
+        "capricornus",
+        "aquarius",
+        "pisces"
+    ]
+
+    @classmethod
+    def get_signs(cls):
+        return cls._signs
+    
+Session.begin()
